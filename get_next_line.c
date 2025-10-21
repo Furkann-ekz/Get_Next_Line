6399bb31f4ad5355b6d66f7ecdf6319b91cd7429
Furkan Ekiz <123004647+Furkann-ekz@users.noreply.github.com>
2025-10-21 14:37:48 +0300

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fekiz <fekiz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 14:10:54 by fekiz             #+#    #+#             */
+/*   Updated: 2025/09/01 13:56:51 by fekiz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*continue_get_st(int len, char *temp, char *st)
+{
+	free(temp);
+	if (len == 0 || len == -1)
+	{
+		if (st && st[0] == '\0')
+		{
+			free(st);
+			return (NULL);
+		}
+		return (st);
+	}
+	free(st);
+	return (NULL);
+}
+
+char	*ft_get_st(int fd, char *st)
+{
+	int		len;
+	char	*temp;
+
+	if (!st)
+	{
+		st = malloc(1);
+		if (st)
+			st[0] = '\0';
+	}
+	temp = malloc(BUFFER_SIZE + 1);
+	if (!temp || !st)
+		return (NULL);
+	while (true)
+	{
+		len = read(fd, temp, BUFFER_SIZE);
+		if (len <= 0)
+			return (continue_get_st(len, temp, st));
+		temp[len] = '\0';
+		st = ft_strjoin(st, temp);
+		if (!st)
+			return (free(temp), NULL);
+		if (search_nl(st) > 0)
+			break ;
+	}
+	return (free(temp), st);
+}
+
+char	*get_new_line(char *st)
+{
+	int		i;
+	char	*line;
+
+	if (!st || st[0] == '\0')
+		return (NULL);
+	i = search_nl(st);
+	if (i > 0)
+		line = malloc(i + 1);
+	else
+		line = malloc(ft_strlen(st) + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (st[i] && st[i] != '\n')
+	{
+		line[i] = st[i];
+		i++;
+	}
+	if (st[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*st;
+	char		*new_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		if (st)
+			free(st);
+		st = NULL;
+		return (NULL);
+	}
+	st = ft_get_st(fd, st);
+	if (!st)
+		return (NULL);
+	new_line = get_new_line(st);
+	if (!new_line)
+		return (free(st), NULL);
+	st = set_new_st(st);
+	return (new_line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fekiz <fekiz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/30 15:32:08 by fekiz             #+#    #+#             */
+/*   Updated: 2025/08/30 15:41:01 by fekiz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	search_nl(char *st)
+{
+	int	i;
+
+	i = 0;
+	if (!st)
+		return (-1);
+	while (st[i])
+	{
+		if (st[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (-1);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static void	ft_next_join(char *s1, char *s2, char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1 && s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2 && s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	free(s1);
+	s1 = NULL;
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+	{
+		s1 = malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	if (!s2)
+		return (free(s1), NULL);
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	str = malloc(i + j + 1);
+	if (!str)
+		return (free(s1), NULL);
+	ft_next_join(s1, s2, str);
+	return (str);
+}
